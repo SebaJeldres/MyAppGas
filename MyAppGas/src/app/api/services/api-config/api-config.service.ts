2ae72b1f6b876c27405b8d1ecdb1fbd6f203eb4d
SebaJeldres <Seba.jeldres@duocuc.cl>
2024-10-14 21:08:48 -0300

    Se hizo algo

---
+++ MyAppGas/src/app/api/services/api-config/api-config.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, filter, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiConfigService {
+
+  baseUrl = environment.api_url;
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'aplication/json',
+      'apiKey': environment.apiKeySupabase,
+      'Authorization': `Bearer ${environment.apiKeySupabase}`
+    });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('Error ocurrido:', error);
+    return throwError(() => error);
+  }
+
+
+  get<T>(path: string, params?: HttpParams): Observable<HttpResponse<T>> {
+    return this.http.get<T>(`${this.baseUrl}/${path}`, { headers: this.getHeaders(), observe: 'response', params })
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  post<T>(path: string, data: any): Observable<HttpResponse<T>> {
+    return this.http.post<T>(
+      `${this.baseUrl}/${path}`,
+      data,
+      {
+        headers: this.getHeaders(),
+        observe: 'response'
+      }
+    ).pipe(
+      catchError(this.handleError)
+    );
+  }
+}
