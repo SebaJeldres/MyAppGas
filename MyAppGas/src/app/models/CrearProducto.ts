6703fe234818d0cd0da3400f1fb730a85667992a
SebaJeldres <Seba.jeldres@duocuc.cl>
2024-11-11 20:59:44 -0300

    Se hizo algo

--- MyAppGas/src/app/models/CrearProducto.ts
+++
@@ -1,9 +0,0 @@
-export interface CrearProducto{
-    sku: number | null;
-    nombre: string;
-    compania: string;
-    tipo: string;
-    kilos: number | null;
-    precio: number | null;
-    stock: number | null;
-    }
