a1bd60ee1241bb0e0b936e08e24f2e6802e5d914
SebaJeldres <Seba.jedlres@duocuc.cl>
2024-11-29 12:29:51 -0300

    se modifico el app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GoogleMapsModule } from '@angular/google-maps'; 
+import { GoogleMapsModule } from '@angular/google-maps';
+import { isPlatform, ionicModule } from '@ionic/angular';
+const getConfig = () =>{
+  if (isPlatform('hybrid')) {
+    return{
+      backButtonText: 'Previous',
+      tabButtonLayout: 'label-hide'
+      }
+    }
 
+    return {
+     menuIcon: 'ellipsis-vertical'
+     }
+  }
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot(getConfig()),
     AppRoutingModule,
     HttpClientModule, // Importa el módulo HTTP aquí
     FormsModule,
